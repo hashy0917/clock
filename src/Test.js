60103eac8b4613289952e962121bd3cb62f589db
hashy0917 <oohasi3939@gmail.com>
2021-03-19 13:02:18 +0900

    asobi

---
+++ src/Test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import hashy0917 from './hashy0917.jpg';
+import './App.css';
+
+function Test() {
+  return (
+      <div className="Test">
+        <header className="App-header">
+          <img src={ hashy0917 } className="hashy0917-icon" alt="icon" />
+        </header>
+      </div>
+  );
+}
+
+export default ;
